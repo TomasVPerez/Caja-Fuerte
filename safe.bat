52ef9ef7ed29673b46d7e641f27dddf18332c329
Tomás <80177664+TomasVPerez@users.noreply.github.com>
2021-08-31 19:45:36 -0300

    Update safe.bat

--- safe.bat
+++ safe.bat
@@ -1,8 +1,7 @@
 @echo Acceso a caja fuerte
 
 @echo off
-cd "C:\Users\Tomas\Desktop\Tomas\Python\safe"
+cd "DIRECTORIO DONDE SE ENCUENTRE GUARDADO cajaFuerte.py"
 set /p modo= "Modo: " 
 set /p clave= "Clave: "
-python safe.py -o %modo% -p %clave%
-cd "C:\Users\Tomas"
+python cajaFuerte.py -o %modo% -p %clave%
